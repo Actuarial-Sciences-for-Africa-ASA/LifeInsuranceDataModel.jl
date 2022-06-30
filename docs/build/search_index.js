d9185178dca94a75a383d320fd34456bb71db1b0
Michael Fliegner <michael.fliegner@hamburg.de>
2022-06-30 16:42:43 +0200

    Documenter dep moved into docs

--- docs/build/search_index.js
+++ docs/build/search_index.js
@@ -1,33 +1,3 @@
-var documenterSearchIndex = {
-  docs: [
-    {
-      location: "#LifeInsuranceDataModel.jl",
-      page: "Home",
-      title: "LifeInsuranceDataModel.jl",
-      text: "",
-      category: "section",
-    },
-    {
-      location: "",
-      page: "Home",
-      title: "Home",
-      text: 'BitemporalPostgres provides an API for application based bitemporal transactions. These are "long" transactions, with persistent transaction data that is, so they serve as  basis of workflow management.',
-      category: "page",
-    },
-    {
-      location: "",
-      page: "Home",
-      title: "Home",
-      text: "For theory behind this, see the theory page.",
-      category: "page",
-    },
-    {
-      location: "#Index",
-      page: "Home",
-      title: "Index",
-      text: "",
-      category: "section",
-    },
-    { location: "", page: "Home", title: "Home", text: "", category: "page" },
-  ],
-};
+var documenterSearchIndex = {"docs":
+[{"location":"#LifeInsuranceDataModel.jl","page":"Home","title":"LifeInsuranceDataModel.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"BitemporalPostgres provides an API for application based bitemporal transactions. These are \"long\" transactions, with persistent transaction data that is, so they serve as  basis of workflow management.","category":"page"},{"location":"","page":"Home","title":"Home","text":"For theory behind this, see the theory page.","category":"page"},{"location":"#Index","page":"Home","title":"Index","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}

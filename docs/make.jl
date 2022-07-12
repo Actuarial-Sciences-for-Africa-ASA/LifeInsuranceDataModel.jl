f19d0400b1df5c14d5a27e80c907dc250b8b9af5
Michael Fliegner <12422482+michaelfliegner@users.noreply.github.com>
2022-07-12 07:04:48 +0200

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -20,5 +20,5 @@ makedocs(
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
 deploydocs(
-    repo="github.com/michaelfliegner/LifeInsuranceDataModel.jl"
+    repo="Actuarial-Sciences-for-Africa-ASA/BitemporalReactive.jl/LifeInsuranceDataModel.jl"
 )

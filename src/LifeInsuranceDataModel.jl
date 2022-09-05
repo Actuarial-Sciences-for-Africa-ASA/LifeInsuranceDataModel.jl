2c89719a88394d285cd6a4a9256c18ff3d2714f8
Kaeptenblaubaer <michelkannfliegen@gmail.com>
2022-09-05 14:19:35 +0000

    new function create_product_instance

--- src/LifeInsuranceDataModel.jl
+++ src/LifeInsuranceDataModel.jl
@@ -158,22 +158,23 @@ ContractSection
 end
 
 """
-create_product_instance(pi::ProductItem, p::Product)
+create_product_instance(wf::Workflow; pi::ProductItem, p::Integer, refp1::Integer,prole1::Integer)
 
     creates tariff items of a productitem pi corresponding to
-    the product parts of a Product p referencing the respective tariffs. 
+    the product parts of a Product p referencing the respective tariffs
+    and Partner refp1 in role prole1
 """
-function create_product_instance(pi::ProductItem, p::Product)
+function create_product_instance(wf::Workflow, pi::ProductItem, p::Integer, refp1::Integer,prole1::Integer)
     map(find(ProductPart, SQLWhereExpression("ref_super=?", p))) do pp
         println(pp.id.value)
         map(find(ProductPartRevision, SQLWhereExpression("ref_component=?", pp.id.value))) do ppr
             println(ppr.description)
-            ti = TariffItem(ref_super=cpi.id)
+            ti = TariffItem(ref_super=pi.id)
             tir = TariffItemRevision(ref_role=ppr.ref_role, ref_tariff=ppr.ref_tariff, description=ppr.description)
-            create_subcomponent!(pi, ti, tir, w4)
+            create_subcomponent!(pi, ti, tir, wf)
             tip = TariffItemPartnerRef(ref_super=pi.id)
-            tipr = TariffItemPartnerRefRevision(ref_partner=Partner1, ref_role=PartnerRole)
-            create_subcomponent!(pi, tip, tipr, w4)
+            tipr = TariffItemPartnerRefRevision(ref_partner=refp1, ref_role=prole1)
+            create_subcomponent!(pi, tip, tipr, wf)
             println(tir)
             println(tipr)
         end

438592711c685be43f72fec5d865bf37346b5324
michaelfliegner <michael.fliegner@hamburg.de>
2023-03-14 15:17:36 +0100

    createTariff interface_id

--- testCreateContract.jl
+++ testCreateContract.jl
@@ -80,19 +80,19 @@ LifeInsuranceDataModel.load_model()
 
     LifeRiskTariff = create_tariff("Life Risk Insurance", 1, "1980 CET - Male Nonsmoker, ANB", tariffparameters)
     TerminalIllnessTariff = create_tariff(
-        "Terminal Illness", 0,
+        "Terminal Illness", 2,
         "2001 VBT Residual Standard Select and Ultimate - Male Nonsmoker, ANB", tariffparameters
     )
     OccupationalDisabilityTariff = create_tariff(
-        "Occupational Disability", 0,
+        "Occupational Disability", 2,
         "2001 VBT Residual Standard Select and Ultimate - Male Nonsmoker, ANB", tariffparameters
     )
     ProfitParticipationTariff = create_tariff(
-        "Profit participation", 0,
+        "Profit participation", 2,
         "2001 VBT Residual Standard Select and Ultimate - Male Nonsmoker, ANB", tariffparameters
     )
     LifeRiskTariff2 = create_tariff(
-        "Two Life Risk Insurance", 0,
+        "Two Life Risk Insurance", 2,
         "2001 VBT Residual Standard Select and Ultimate - Male Nonsmoker, ANB", tariffparameters, [1, 2])
 
     find(TariffRevision)
